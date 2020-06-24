e48b05ce6af0f953429336b5bdf8d1a4c167c5c4
Mati <bojanowski.mateusz@gmail.com>
2020-06-24 13:41:03 +0200

    Update script for starting stoping infrastructure

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+echo Create k8s cluster:
 minikube start --driver=docker
+
+echo Create new namespace argo:
 kubectl create namespace argo
+
+echo Apply all changes argo/mino:
+sleep  15
 kubectl apply -n argo -f install.yaml
 
-sleep 30
+
+echo Setup port forwarding for mino an argo server:
+sleep 60
 kubectl port-forward -n argo svc/argo-server 8080:2746 &
 kubectl port-forward -n argo svc/minio 9000:9000 &
+
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo Remove k8s cluster:
 minikube delete
